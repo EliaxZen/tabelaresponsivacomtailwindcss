6e8363373e793253288cc507cfde77a69878abbd
Elias Galvão Alves <132005740+EliaxZen@users.noreply.github.com>
2023-11-22 09:49:25 -0300

    arquivos principais

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,js}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'vermelho': '#FF0000',
+        'vermelhoescuro': '#990000'
+      }
+    },
+  },
+  plugins: [],
+}
+
